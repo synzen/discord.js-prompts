3ed74ed60aa13db53e73a9c4e44bcc03c40be053
synzen <michael994664@gmail.com>
2020-04-09 11:45:30 -0400

    Update DiscordPrompt to use abstract methods

--- src/structs/DiscordPrompt.ts
+++ src/structs/DiscordPrompt.ts
@@ -1,3 +1,4 @@
+import { Message } from 'discord.js'
 import { Prompt, PromptCollector, Rejection } from "prompt-anything";
 import { EventEmitter } from 'events'
 import { DiscordChannel } from "./DiscordChannel";
@@ -11,29 +12,37 @@ export class DiscordPrompt<T extends BaseData> extends Prompt<T> {
   static inactivityFormat: MessageFormat = {
     text: 'Menu has been closed due to inactivity.'
   }
+  static exitFormat: MessageFormat = {
+    text: 'Menu closed.'
+  }
   static getRejectFormat (error: Rejection): MessageFormat {
     return {
       text: error.message
     }
+  }
+  async onReject(message: Message, error: Rejection, channel: DiscordChannel, data: T): Promise<void> {
+    await this.sendMessage(DiscordPrompt.getRejectFormat(error), channel)
+  }
+  async onInactivity(channel: DiscordChannel, data: T): Promise<void> {
+    await this.sendMessage(DiscordPrompt.inactivityFormat, channel)
+  }
+  async onExit(message: Message, channel: DiscordChannel, data: T): Promise<void> {
+    await this.sendMessage(DiscordPrompt.exitFormat, channel)
   }
 
   createCollector(channel: DiscordChannel, data: T): PromptCollector<T> {
     const discordChannel = channel
     const emitter: PromptCollector<T> = new EventEmitter()
     const collector = discordChannel.channel.createMessageCollector(m => m.author.id === data.authorID);
-    collector.on('collect', async message => {
-      emitter.emit('message', message)
+    collector.on('collect', async (message: Message) => {
+      if (message.content === 'exit') {
+        emitter.emit('exit', message)
+      } else {
+        emitter.emit('message', message)
+      }
     });
     emitter.once('stop', () => {
       collector.stop()
-    })
-    emitter.on('reject', (message, error) => {
-      this.sendMessage(DiscordPrompt.getRejectFormat(error), channel)
-        .catch(err => emitter.emit('error', err))
-    })
-    emitter.once('inactivity', () => {
-      this.sendMessage(DiscordPrompt.inactivityFormat, channel)
-        .catch(err => emitter.emit('error', err))
     })
     return emitter
   }  
